684d7ae266a70667630aad7d668a3942cde4a651
Emily <emilymfeuer@gmail.com>
2019-02-15 17:34:14 -0500

    FourBar position changes
    Start position command group added, buttons placed in OI

---
+++ roboToe/src/main/java/frc/robot/commands/FourBarStartGroup.java
@@ -0,0 +1,21 @@
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class FourBarStartGroup extends CommandGroup{
+
+  public FourBarStartGroup(boolean fourBarStart, boolean wristUp){
+    
+    int startPoint = 5;
+
+
+    
+  
+      //addSequential(new Drive4Distance(Constants.level2Length));
+      //addSequential(new Drive4Time(0.25, -0.5));
+
+  
+  }
+
+}
