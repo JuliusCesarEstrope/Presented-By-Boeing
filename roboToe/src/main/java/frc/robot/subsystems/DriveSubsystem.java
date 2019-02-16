60468ec1ceff046f71786de3e69aa5649dcb0be3
Salvatore <smazzola007@gmail.com>
2019-02-16 12:45:49 -0500

    Update DriveSubsystem.java

--- roboToe/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ roboToe/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -28,13 +28,7 @@ public class DriveSubsystem extends Subsystem {
   private static PIDController gyroPID;
   private static PIDOutput gyroPIDOutput;
 
-<<<<<<< HEAD
-public DriveSubsystem(int[] motorPortsLeft, int[] motorPortsRight, int gyroPort, int encoderPortLeft[], int encoderPortRight[],int frontSensor, int rightSensor, int backSensor, int leftSensor, int[] leftEncoderPorts, int[] rightEncoderPorts, double circumferanceOfWheels, double ticksOfEncoder){
-  this.driveEnabled = driveEnabled;
-
-=======
 public DriveSubsystem(int[] motorPortsLeft, int[] motorPortsRight, int gyroPort, int[] driveEncoderPortLeft, int[] driveEncoderPortRight,int frontSensor, int rightSensor, int backSensor, int leftSensor, double circumferenceOfWheels, double ticksOfEncoder){
->>>>>>> master
   gyroDrive = new AnalogGyro(gyroPort);
   encoderLeft = new Encoder(driveEncoderPortLeft[0],driveEncoderPortLeft[1]);
   encoderRight = new Encoder(driveEncoderPortRight[0],driveEncoderPortRight[1]);
@@ -47,50 +41,13 @@ public DriveSubsystem(int[] motorPortsLeft, int[] motorPortsRight, int gyroPort,
   victorsRight = new WPI_VictorSPX[motorPortsRight.length - 1];
 
   for(int i = 0; i < victorsLeft.length; i++)
-<<<<<<< HEAD
-    victorsLeft[i] = new WPI_VictorSPX(motorPortsLeft[i+1]); 
-=======
     victorsLeft[i] = new WPI_VictorSPX(motorPortsLeft[i]); 
 
->>>>>>> master
   for(int i = 0; i < victorsRight.length; i++)
     victorsRight[i] = new WPI_VictorSPX(motorPortsRight[i+1]);
   
-<<<<<<< HEAD
-    
-      talonLeft = new WPI_TalonSRX(motorPortsLeft[0]);
-      talonRight = new WPI_TalonSRX(motorPortsRight[0]);
-    
-
-      encoderLeft.setDistancePerPulse(circumferanceOfWheels/ticksOfEncoder);
-      
-  }
-  
-  public void resetGyro() {
-		if (driveEnabled) {
-			driveGyro.reset();
-		}
-  }
-
-  public void setgyroPIDValues(double p, double i, double d){
-    gyroPID.setPID(p, i, d);
-  }
-
-  public void setGyroSetpoint(double setpoint){
-    gyroPID.setSetpoint(setpoint);
-  }
-
-  public boolean gyroPIDOnSetpoint(){
-    return gyroPID.onTarget();
-  }
-
-  public double getGyroPIDOutput(){
-    return gyroPID.get();
-  }
-=======
   for (int i = 0; i < victorsLeft.length; i++) 
     victorsLeft[i] = new VictorSPX(motorPortsLeft[i]);
->>>>>>> master
 
   for (int i = 0; i < victorsRight.length; i++) 
     victorsRight[i] = new VictorSPX(motorPortsRight[i]);
@@ -118,22 +75,6 @@ public void setBoth(double speed){
 public void calibrateGyro(){
   gyroDrive.calibrate();
 }
-<<<<<<< HEAD
-
-public double getAngle(){
-    return gyroDrive.getAngle() % 360;
-}
-public void resetAngle() {
-  gyro.reset();
-}
-public void resetEncoder(){
-  encoderLeft.reset();
-  encoderRight.reset();
-}
-public int getEncderLeft(){
-  return encoderLeft.get();
-}
-=======
 public void resetGyro(){
     gyroDrive.reset();
   }
@@ -141,7 +82,6 @@ public void resetGyro(){
   public double getAngle() {
     return gyroDrive.getAngle() % 360;
   }
->>>>>>> master
 
   public void resetEncoder() {
     encoderLeft.reset();
@@ -156,8 +96,6 @@ public void resetGyro(){
     return encoderRight.get();
   }
 
-<<<<<<< HEAD
-=======
   @Override
   public void initDefaultCommand() {
     setDefaultCommand(new TankDrive());
@@ -169,7 +107,6 @@ public void resetAngle() {
   gyroDrive.reset();
 }
 
->>>>>>> master
 public double getWheelDistanceLeft() {
   return encoderLeft.getDistance();
 }
@@ -194,8 +131,4 @@ public boolean getBackSensor(){
 }
 
 //sets rightSensor to a boolean value of true being it is seeing the tape
-<<<<<<< HEAD
-  }
-=======
 }
->>>>>>> master
