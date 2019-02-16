79c35a6dd7fd475b2bdb92d2a6a96cb7f1271e84
Salvatore <smazzola007@gmail.com>
2019-02-16 13:54:12 -0500

    Update Auton.java
    Uncommented all of the TurnAngle and Drive4Time commands.

--- roboToe/src/main/java/frc/robot/autonomi/Auton.java
+++ roboToe/src/main/java/frc/robot/autonomi/Auton.java
@@ -17,12 +17,12 @@ public class Auton extends CommandGroup{
 
     if(onLevel2){
 
-      //addSequential(new Drive4Distance(Constants.level2Length));
+      //addSequential(new DriveForDistanceCommand(Constants.level2Length));
       addSequential(new DriveForTimeCommand(0.25, -0.5, -0.5));
 
     }
 
-    //addSequential(new Drive4Distance(Constants.level1Length + Constants.firstExtra));
+    //addSequential(new DriveForDistanceCommand(Constants.level1Length + Constants.firstExtra));
 
     if(startMiddle){
       
@@ -32,15 +32,15 @@ public class Auton extends CommandGroup{
         angleModifier = -1;
 
         addSequential(new TurnAngleCommand(Constants.midToMidAngle * angleModifier));
-        //addSequential(new Drive4Distance(Constants.midToMidDistance));
+        //addSequential(new DriveForDistanceCommand(Constants.midToMidDistance));
         addSequential(new TurnAngleCommand(-Constants.midToMidAngle * angleModifier));
 
       } else{
 
         addSequential(new TurnAngleCommand(Constants.midToSideAngle * angleModifier));
-        //addSequential(new Drive4Distance(Constants.midToSideDistance));
+        //addSequential(new DriveForDistanceCommand(Constants.midToSideDistance));
         addSequential(new TurnAngleCommand(-Constants.midToSideAngle * angleModifier));
-        //addSequential(new Drive4Distance(distanceToTarget));
+        //addSequential(new DriveForDistanceCommand(distanceToTarget));
         addSequential(new TurnAngleCommand(90 * angleModifier));
 
       }
@@ -55,13 +55,13 @@ public class Auton extends CommandGroup{
         if(distanceToTarget == 0){
 
           addSequential(new TurnAngleCommand(Constants.sideToCloseMidAngle * angleModifier));
-          //addSequential(new Drive4Distance(Constants.sideToCloseMidDistance));
+          //addSequential(new DriveForDistanceCommand(Constants.sideToCloseMidDistance));
           addSequential(new TurnAngleCommand(-Constants.sideToCloseMidAngle * angleModifier));
 
         } else {
           
           addSequential(new TurnAngleCommand(Constants.sideToFarMidAngle * angleModifier));
-          //addSequential(new Drive4Distance(Constants.sideToFarMidDistance));
+          //addSequential(new DriveForDistanceCommand(Constants.sideToFarMidDistance));
           addSequential(new TurnAngleCommand(-Constants.sideToFarMidAngle * angleModifier));
 
         }
@@ -72,26 +72,26 @@ public class Auton extends CommandGroup{
 
           angleModifier2 = -1;
           addSequential(new TurnAngleCommand(Constants.crossAngle * angleModifier));
-          //addSequential(new Drive4Distance(Constants.crossDistance));
+          //addSequential(new DriveForDistanceCommand(Constants.crossDistance));
           addSequential(new TurnAngleCommand(-Constants.crossAngle * angleModifier));
 
         } else {
 
           angleModifier2 = 1;
           addSequential(new TurnAngleCommand(Constants.sameSideAngle * angleModifier));
-          //addSequential(new Drive4Distance(Constants.sameSidesDistance));
+          //addSequential(new DriveForDistanceCommand(Constants.sameSidesDistance));
           addSequential(new TurnAngleCommand(-Constants.sameSideAngle * angleModifier));
 
         }
 
-        //addSequential(new Drive4Distance(distanceToTarget));
+        //addSequential(new DriveForDistanceCommand(distanceToTarget));
         addSequential(new TurnAngleCommand(90 * angleModifier * angleModifier2));
 
       }
         
     }
 
-    //addSequential(new Drive4Distance(Constants.distanceToHatch));
+    //addSequential(new DriveForDistanceCommand(Constants.distanceToHatch));
     addSequential(new BooperAutonCommand());
 
   }
