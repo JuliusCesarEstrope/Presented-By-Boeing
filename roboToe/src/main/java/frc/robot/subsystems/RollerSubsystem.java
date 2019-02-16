b708c7838a2081d3a933539afb208f17883ee812
msimons6303 <msimons6303@gmail.com>
2019-02-16 14:16:42 -0500

    Roller Subsystem
    Fixed roller command name error in roller subsystem

--- roboToe/src/main/java/frc/robot/subsystems/RollerSubsystem.java
+++ roboToe/src/main/java/frc/robot/subsystems/RollerSubsystem.java
@@ -5,7 +5,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.commands.Roller;
+import frc.robot.commands.RollerCommand;
 import frc.robot.utilities.RobotLog;
 
 public class RollerSubsystem extends Subsystem {
@@ -22,6 +22,6 @@ public class RollerSubsystem extends Subsystem {
   }
 
   public void initDefaultCommand() {
-    setDefaultCommand(new Roller());
+    setDefaultCommand(new RollerCommand());
   }
 }
