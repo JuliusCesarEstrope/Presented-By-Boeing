55592d08bf8439546570c35f8a96b49dd8de18d8
Salvatore <smazzola007@gmail.com>
2019-03-19 17:21:20 -0400

    Update WristSubsystem.java
    Changed some small spacing

--- roboToe/src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ roboToe/src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -46,7 +46,6 @@ public class WristSubsystem extends Subsystem {
       rightWristMotor.config_kD(0, wristPIDValues[2]);
       rightWristMotor.config_kF(0, wristPIDValues[3]);
 
-      leftWristMotor.limit
     /*leftWristPID = new PIDController(wristPIDValues[0], wristPIDValues[1], wristPIDValues[2], wristPIDValues[3],
         leftWristEncoder, pidWristOutput);
     leftWristPID.setEnabled(true);
