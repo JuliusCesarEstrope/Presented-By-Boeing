211cfd508ead0ceebeb6f0db2e0d83792064011a
msimons6303 <msimons6303@gmail.com>
2019-03-21 18:21:48 -0400

    Auton Elevator

--- roboToe/src/main/java/frc/robot/subsystems/ElevatorHorizontalSubsystem.java
+++ roboToe/src/main/java/frc/robot/subsystems/ElevatorHorizontalSubsystem.java
@@ -1,67 +1,44 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.Constants;
+import frc.robot.commands.ElevatorInPlaceCommand;
 import frc.robot.utilities.RobotLog;
 
 public class ElevatorHorizontalSubsystem extends Subsystem {
-  static VictorSPX leftXElevatorMotor, rightXElevatorMotor;
-  static DigitalInput fastSlowStopSensor, limitSwitch;
+  static TalonSRX leftXElevatorMotor, rightXElevatorMotor;
+  static DigitalInput fastSlowStopSensor;
 
-  public ElevatorHorizontalSubsystem(int leftXElevatorMotorPort,
+  public ElevatorHorizontalSubsystem(int leftXElevatorMotorPort, int rightXElevatorMotorPort,
       double defaultElevatorPosition) {
 
-        if (Constants.wristEnabled){
+    leftXElevatorMotor = new WPI_TalonSRX(leftXElevatorMotorPort);
+    rightXElevatorMotor = new WPI_TalonSRX(rightXElevatorMotorPort);
 
-    leftXElevatorMotor = new WPI_VictorSPX(leftXElevatorMotorPort);
-    limitSwitch = new DigitalInput(Constants.limitSwitch);
-/*
     rightXElevatorMotor.follow(leftXElevatorMotor);
-    rightXElevatorMotor.setInverted(true); //SHOULD BE HERE?
-*/
+    rightXElevatorMotor.setInverted(true);
+
     RobotLog.putMessage("Running ElevatorHorizontalSubsystem");
-        }
   }
 
   public void setXElevatorMotors(double Speed) {
-    if (Constants.wristEnabled){
     leftXElevatorMotor.set(ControlMode.PercentOutput, Speed);
-    }
   }
 
   public double getXElevatorMotors(){
-    if (Constants.wristEnabled){
     return leftXElevatorMotor.getMotorOutputPercent();
-    } else {
-      return 0;
-    }
   }
 
   public boolean getfastSlowStopSensor() {
-    if (Constants.wristEnabled){
     return fastSlowStopSensor.get();
-    } else {
-      return false;
-    }
-  }
-
-  public boolean getlimitSwitch(){
-    if (Constants.wristEnabled){
-    return limitSwitch.get();
-    } else {
-      return false;
-    }
   }
 
   public void initDefaultCommand() {
-    if (Constants.wristEnabled){
-    //setDefaultCommand(new ElevatorInPlaceCommand());
-    }
+    setDefaultCommand(new());
   }
 
 }
