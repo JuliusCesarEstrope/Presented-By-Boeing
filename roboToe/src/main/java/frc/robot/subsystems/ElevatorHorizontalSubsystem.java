befe87f784f14f25def420e786104231165e0afe
msimons6303 <msimons6303@gmail.com>
2019-03-25 19:06:31 -0400

    ye

--- roboToe/src/main/java/frc/robot/subsystems/ElevatorHorizontalSubsystem.java
+++ roboToe/src/main/java/frc/robot/subsystems/ElevatorHorizontalSubsystem.java
@@ -10,6 +10,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Constants;
 import frc.robot.utilities.RobotLog;
@@ -18,10 +20,12 @@ public class ElevatorHorizontalSubsystem extends Subsystem {
   static TalonSRX XElevatorMotor;
   static DigitalInput fastSlowStopSensor, limitSwitch;
   static Encoder XElevatorEncoder;
+  private static PIDController XElevatorPID;
+  private static PIDOutput encoderPIDOutput;
+  private static int tolerance = 70;
 
   public ElevatorHorizontalSubsystem(int XElevatorMotorPort,
-      double defaultElevatorPosition, int XElevatorEncoderPort) {
-
+      double defaultElevatorPosition, int XElevatorEncoderPort, double[] XElevatorEncoderPIDValues) {
         if (Constants.wristEnabled){
 
     XElevatorMotor = new WPI_TalonSRX(XElevatorMotorPort);
@@ -29,11 +33,18 @@ public class ElevatorHorizontalSubsystem extends Subsystem {
     XElevatorMotor.configFactoryDefault();
     XElevatorMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
 
+    XElevatorMotor.config_kP(0, XElevatorEncoderPIDValues[0]);
+    XElevatorMotor.config_kI(0, XElevatorEncoderPIDValues[1]);
+    XElevatorMotor.config_kD(0, XElevatorEncoderPIDValues[2]);
+    XElevatorMotor.config_kF(0, XElevatorEncoderPIDValues[3]);
+
    // XElevatorEncoder.configPeakOutputForward(1);
    // XElevatorEncoder.configPeakOutputReverse(-1);
    // XElevatorEncoder.configClosedloopRamp(ramp);
 
    // XElevatorEncoder.setInverted(true);
+   
+    XElevatorPID = new PIDController(0.05, 0, 0, XElevatorEncoder, encoderPIDOutput);
 
     XElevatorMotor.setNeutralMode(NeutralMode.Brake);
 
@@ -55,6 +66,38 @@ public class ElevatorHorizontalSubsystem extends Subsystem {
     }
   }
 
+  public double getXElevatorEncoder(){
+    if (Constants.wristEnabled){
+      return XElevatorEncoder.get();
+      } else {
+        return 0;
+      }
+  }
+
+  public void setEncoder(double speed) {
+    if (Constants.wristEnabled) {
+      XElevatorMotor.set(ControlMode.PercentOutput, speed);
+    }
+  }
+
+  public void setLeftMotorPosition(double position) {
+    if (Constants.wristEnabled) {
+      XElevatorMotor.set(ControlMode.Position, position);
+    }
+  }
+
+  public boolean encoderOnTarget() {
+    if (Constants.wristEnabled) {
+      return Math.abs(XElevatorMotor.getClosedLoopError()) < tolerance;
+    } else {
+      return false;
+    }
+  }
+
+  public void encoderReset(){
+    XElevatorEncoder.reset();
+  }
+
   public boolean getfastSlowStopSensor() {
     if (Constants.wristEnabled){
     return fastSlowStopSensor.get();
@@ -69,14 +112,6 @@ public class ElevatorHorizontalSubsystem extends Subsystem {
     } else {
       return false;
     }
-  }
-
-  public double getXElevatorEncoder(){
-    if (Constants.wristEnabled){
-      return XElevatorEncoder.get();
-      } else {
-        return 0;
-      }
   }
 
   public void initDefaultCommand() {
