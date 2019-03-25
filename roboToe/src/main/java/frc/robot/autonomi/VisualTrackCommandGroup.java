3748093bc19034e9469e005bc989aae84c3e4d01
Emily <emilymfeuer@gmail.com>
2019-03-25 17:32:02 -0400

    Created VisualTrackCommandGroup

---
+++ roboToe/src/main/java/frc/robot/autonomi/VisualTrackCommandGroup.java
@@ -0,0 +1,14 @@
+
+package frc.robot.autonomi;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class VisualTrackCommandGroup extends CommandGroup{
+
+  public VisualTrackCommandGroup(){
+      
+     
+     
+  }
+
+}
