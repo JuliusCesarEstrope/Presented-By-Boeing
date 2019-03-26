38cf6fbb71d09de070df50012224d43630266287
Emily <emilymfeuer@gmail.com>
2019-03-26 17:40:59 -0400

    Added match timer to LEDLightSubsystem

--- roboToe/src/main/java/frc/robot/subsystems/LEDLightSubsystem.java
+++ roboToe/src/main/java/frc/robot/subsystems/LEDLightSubsystem.java
@@ -3,11 +3,14 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Timer;
 
 public class LEDLightSubsystem extends Subsystem {
   static Spark LEDLight;
+  static Timer timer;
 
   public LEDLightSubsystem() {
+    timer = new Timer();
     if (Constants.wristEnabled){
     //LEDLight = new Spark(-1);
     }
@@ -17,6 +20,11 @@ public class LEDLightSubsystem extends Subsystem {
     if (Constants.wristEnabled){
     //LEDLight.set(Speed);
     }
+  }
+
+  public static double getMatchTime() { 
+    return LEDLightSubsystem.getMatchTime();
+
   }
 
   // Blue nothing align(default), Green isfully aligned, red not aligned, yellow is close to align but not yet
