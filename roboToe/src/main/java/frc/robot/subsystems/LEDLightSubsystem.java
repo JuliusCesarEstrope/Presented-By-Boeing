066f6b8aaaf6acd1bdbd0779fac6022b68358fac
Emily <emilymfeuer@gmail.com>
2019-03-26 17:55:48 -0400

    Removed timer for led light????
    didn't mean to add this in master oops

--- roboToe/src/main/java/frc/robot/subsystems/LEDLightSubsystem.java
+++ roboToe/src/main/java/frc/robot/subsystems/LEDLightSubsystem.java
@@ -3,14 +3,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Constants;
-import edu.wpi.first.wpilibj.Timer;
 
 public class LEDLightSubsystem extends Subsystem {
   static Spark LEDLight;
-  static Timer timer;
 
   public LEDLightSubsystem() {
-    timer = new Timer();
     if (Constants.wristEnabled){
     //LEDLight = new Spark(-1);
     }
@@ -22,10 +19,9 @@ public class LEDLightSubsystem extends Subsystem {
     }
   }
 
-  public static double getMatchTime() { 
-    return LEDLightSubsystem.getMatchTime();
+ 
 
-  }
+
 
   // Blue nothing align(default), Green isfully aligned, red not aligned, yellow is close to align but not yet
 
